346a1bb3b9172f3a0e58a0e3b96641daf623a256
notageek-guy <manishkumar97430@gmail.com>
2022-10-13 16:11:24 +0530

    removed log

--- src/App.jsx
+++ src/App.jsx
@@ -16,47 +16,55 @@ export default function App() {
   };
   const addColor = () => {
     let newId = new Date().getTime();
-    
+
     let ChangedColor = stc(color);
-    if(color.length > 0){
-    setColorList((prev) => {
-      return [...prev, { id: newId, name:  color, color: ChangedColor }];
-    });
-    setColor("");
+    if (color.length > 0) {
+      setColorList((prev) => {
+        let filtered = prev.filter((item) => item.color !== ChangedColor);
+        return [
+          ...filtered,
+          {
+            id: newId,
+            name: color,
+            color: ChangedColor,
+          },
+        ];
+      });
+      setColor("");
     }
   };
-  console.log(colorList);
+
+  // console.log(colorList);
   const genRandom = () => {
     let newId = new Date().getTime();
     let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
     setColorList((prev) => {
       return [...prev, { id: newId, name: "Random", color: randomColor }];
     });
-  }
+  };
   return (
-    <VStack p={4} overflow='hidden'>
-      <HStack alignSelf={'flex-end'}>
-      <Github/>
-      <DarkModeToggle />
+    <VStack p={4} overflow="hidden">
+      <HStack alignSelf={"flex-end"}>
+        <Github />
+        <DarkModeToggle />
       </HStack>
       <Heading
         as="h1"
         bgClip="text"
-        size='2xl'
+        size="2xl"
         fontSize={{
           base: "2xl",
           sm: "3xl",
           md: "4xl",
           lg: "5xl",
         }}
-
-        bgGradient='linear(to-l, #7928CA, #FF0080)'
+        bgGradient="linear(to-l, #7928CA, #FF0080)"
       >
         React Pallete
       </Heading>
       <ColorAdder color={color} addColor={addColor} handleInput={handleInput} />
-      <Pallete  colorList={colorList}/>
-      <ColorGenerator genRandom={genRandom}/>
+      <Pallete colorList={colorList} />
+      <ColorGenerator genRandom={genRandom} />
     </VStack>
   );
 }
--- src/components/Github.jsx
+++ src/components/Github.jsx
@@ -4,7 +4,7 @@ import { FaGithub } from "react-icons/fa";
 export default function Github() {
   return (
     
-      <IconButton as="a" href="" alignSelf={'flex-end'}  icon={<FaGithub />} />
+      <IconButton as="a" href="https://github.com/notageek-guy/color-pallete-react" alignSelf={'flex-end'}  icon={<FaGithub />} />
     
   );
 }
